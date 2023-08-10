54199938d605c0924aa0a5210657cb416ac590a8
Matheus Mascarenhas <mattmascarenhas7@gmail.com>
2023-08-10 09:53:02 -0300

    google functions tamplate zip

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using Google.Cloud.Functions.Framework;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.IO;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace HelloHttp;
+
+public class Function  {
+    static async Task Main(string[] args) {
+        Console.WriteLine("wow");
+        CreateHostBuilder(args).Build().Run();
+
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+           Host.CreateDefaultBuilder(args)
+               .ConfigureWebHostDefaults(webBuilder => {
+                   webBuilder.UseStartup<Startup>();
+                   webBuilder.UseUrls("http://0.0.0.0:8080");  // Define o host e a porta aqui
+               });
+}
+
+public class Startup {
+    public Startup(IConfiguration configuration) {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration {
+        get;
+    }
+
+    public void ConfigureServices(IServiceCollection services) {
+        services.AddControllers();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+        if (env.IsDevelopment()) {
+            app.UseDeveloperExceptionPage();
+        } else {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints => {
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+        });
+    }
+}
